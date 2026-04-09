7a33762b83a431bf132a183ae5f7359c1e3b9b02
anametec <anacarolinathbg@gmail.com>
2026-04-09 20:17:28 +0000

    aula01 -base do projeto

---
+++ aulaAPI/src/App.jsx
@@ -0,0 +1,21 @@
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/home'
+import Pokemon from './pages/pokemon'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/pokemon">Pokémon</Link>
+      </nav>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/pokemon" element={<Pokemon/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ aulaAPI/src/pages/pokemon/index.jsx
@@ -0,0 +1,9 @@
+function Pokemon(){
+    return (
+        <div>
+            <h1>Lista de Pokémon</h1>
+        </div>
+    )
+}
+
+export default Pokemon
