d7ee19105fae358a87b58b6a9f9640b937e054dc
anametec <anacarolinathbg@gmail.com>
2026-04-09 20:27:36 +0000

    aula01 -lista de pokemon

--- aulaAPI/src/pages/pokemon/index.jsx
+++ aulaAPI/src/pages/pokemon/index.jsx
@@ -1,3 +1,14 @@
+import {useState} from 'react'
+
+const pokemonList = [
+    {id:1, nome:'Bulbasauro'},
+    {id:4, nome:'Charmander'},
+    {id:22, nome:'Rattata'},
+    {id:333, nome:'Swablu'},
+    {id:7, nome:'Squirtle'},
+    {id:13, nome:'Weedle'}
+]
+
 function Pokemon(){
     return (
         <div>
