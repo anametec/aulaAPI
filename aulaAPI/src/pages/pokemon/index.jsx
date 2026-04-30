7ca0610c7a574aecd5efa177a378393a232f8622
anametec <anacarolinathbg@gmail.com>
2026-04-30 18:21:58 +0000

    -informação do pokemon

--- aulaAPI/src/pages/pokemon/index.jsx
+++ aulaAPI/src/pages/pokemon/index.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react'
+import { useState } from 'react'
+import './style.css'
 
 const pokemonList = [
     {id:1, nome:'Bulbasauro'},
@@ -6,14 +7,65 @@ const pokemonList = [
     {id:22, nome:'Rattata'},
     {id:333, nome:'Swablu'},
     {id:7, nome:'Squirtle'},
-    {id:13, nome:'Weedle'}
+    {id:13, nome:'Weedle'},
+    {id:5, nome:'Charmeleon'},
+    {id:24, nome:'Arbok'},
+    {id:2, nome:'Ivysaur'},
+    {id:23, nome:'Ekans'},
+    {id:18, nome:'Pidgeot'}
 ]
 
 function Pokemon(){
+    const [pokemonGlobal, setPokemonGlobal] = useState(null)
+
+    const getPokemonData = (idPokemon)=>{
+        const uri = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`
+
+        fetch(uri)
+            .then(res =>res.json())
+                .then(json=>{
+                    const pokemonFetch = {
+                        nome: json.name,
+                        altura: json.height,
+                        peso: json.weight,
+                        vida: json.stats[0].base_stat,
+                        geracao: json.generation,
+                        imagem: json.sprites.other['official-artwork'].front_default
+                    }
+                    setPokemonGlobal(pokemonFetch)
+                    console.log(pokemonFetch)
+                })
+                .catch(()=>alert('Não foi possível acessar os dados do pokémon'))
+    }
+
     return (
-        <div>
-            <h1>Lista de Pokémon</h1>
+        <div className='container'>
+            <div className='header'>
+                <h1>Escolha o seu Pokémon</h1>
+            </div>
+
+            {pokemonGlobal && (
+                <div className='pokemon-info'>
+                    <h2>Nome: {pokemonGlobal.nome}</h2>
+                    <h2>Altura: {pokemonGlobal.altura}</h2>
+                    <p>Peso: {pokemonGlobal.peso}</p>
+                    <p>Vida: {pokemonGlobal.vida}</p>
+                    <p>Geração: {pokemonGlobal.generation}</p>
+                    <img
+                        src={pokemonGlobal.imagem}
+                        alt={pokemonGlobal.nome}
+                        />
+                </div>
+            )}
+
+            {pokemonList.map((item)=>(
+                <div className='card' key={item.id}>
+                    <p>{item.nome}</p>
+                    <button onClick={()=>getPokemonData(item.id)}>Saiba mais</button>
+                </div>
+            ))}
         </div>
+   
     )
 }
 
